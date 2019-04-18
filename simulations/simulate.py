d1049c5a64626550ac2fbd65315aa23da1a1556f
Ryan Harter <harterrt@gmail.com>
2019-04-18 18:44:30 -0400

    Clean up formatting for flake8

--- simulations/simulate.py
+++ simulations/simulate.py
@@ -3,15 +3,20 @@ import numpy as np
 
 CI = namedtuple('ConfidenceInterval', ['point', 'low', 'high'])
 
+
 def resample(data, random_state=None):
-    return(data.sample(replace=True, n=len(data.index), random_state=random_state))
+    return(data.sample(
+        replace=True,
+        n=len(data.index),
+        random_state=random_state
+    ))
 
 
 def bootstrap(data, function, iterations=100):
     '''Calculates 95% CI for `function(data)`
-    
+
     data:       A pandas DataFrame()
-    function:   A function that takes a pandas DataFrame() and returns a numeric
+    function:   A function that takes a pd.DataFrame() and returns a numeric
     iterations: Number of iterations to run. Defaults to 1000.
     '''
     point_estimate = function(data)
@@ -37,14 +42,15 @@ def ci_contains(ci, point):
 
 
 def eval_bootstrap(function, population, sample_size, meta_iter=100):
-    '''Test the effectiveness of Bootstrapping 
+    '''Test the effectiveness of Bootstrapping
 
     To test the effectiveness of bootstrapping for the given population,
-    we take `meta_iter` samples from `population` with `sample_size` observations.
-    We then calculate the bootstrap CI for `function(sample)` for each sample
-    and report whether the CI's include the true value of `function(population)`
+    we take `meta_iter` samples from `population` with `sample_size`
+    observations. We then calculate the bootstrap CI for `function(sample)`
+    for each sample and report whether the CI's include the true value of
+    `function(population)`
 
-    function:    A function that takes a pandas DataFrame() and returns a numeric
+    function:    A function that takes a pd.DataFrame() and returns a numeric
     population:  A DataFrame()
     sample_size: An int
     meta_iter:   An int
