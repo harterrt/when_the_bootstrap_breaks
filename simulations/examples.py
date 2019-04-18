d06aca5229d755a0ff9c4d99f3852b4dc6d99ee1
Ryan Harter <harterrt@gmail.com>
2019-04-18 18:44:30 -0400

    Clean up formatting for flake8

--- simulations/examples.py
+++ simulations/examples.py
@@ -2,9 +2,12 @@ import simulate as sim
 import pandas as pd
 import numpy as np
 
-coin_flip = pd.DataFrame({'x':[0, 1]})
-mean = lambda x: np.mean(x)[0]
 
+def mean(x):
+    return(np.mean(x)[0])
+
+
+coin_flip = pd.DataFrame({'x': [0, 1]})
 bootstrap_results = sim.eval_bootstrap(mean, coin_flip, 100)
 
 # Success rate
